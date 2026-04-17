da77c96d92c21f481da3358d3b324caf7290d909
abusazid46-tech <abusazid46@gmail.com>
2026-04-17 08:54:04 -0700

    Add new services to the backend init file

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,9 +1,19 @@
 from .auth_service import auth_service
 from .user_service import user_service
 from .menu_service import menu_service
+from .cart_service import cart_service
+from .order_service import order_service
+from .payment_service import payment_service
+from .reservation_service import reservation_service
+from .review_service import review_service
 
 __all__ = [
     "auth_service",
     "user_service",
-    "menu_service"
+    "menu_service",
+    "cart_service",
+    "order_service",
+    "payment_service",
+    "reservation_service",
+    "review_service"
 ]
