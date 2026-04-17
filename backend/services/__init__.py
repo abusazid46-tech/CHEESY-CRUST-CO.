422c21fd8542e5cec726d1199d5c7a3045dd7103
abusazid46-tech <abusazid46@gmail.com>
2026-04-17 08:48:41 -0700

    Initialize services module with imports

---
+++ backend/services/__init__.py
@@ -0,0 +1,9 @@
+from .auth_service import auth_service
+from .user_service import user_service
+from .menu_service import menu_service
+
+__all__ = [
+    "auth_service",
+    "user_service",
+    "menu_service"
+]
